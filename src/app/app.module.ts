0284650b1bae39dc2d30091707de435cb39cf408
AlbertClemente <40822559+AlbertClemente@users.noreply.github.com>
2018-07-26 11:59:10 +0200

    Prova forms i prova component forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { SigninFormComponent } from './signin-form/signin-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SigninFormComponent
   ],
   imports: [
     BrowserModule
